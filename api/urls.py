f5064ba494188e365952a04c677fcc291f50ff74
Victor <victorfariasbezerra12@gmail.com>
2025-12-04 17:40:20 -0300

    JWT

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from api.views import StartTaskView, ProgressView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # JWT Authentication
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
     path("gpon-conversor/", StartTaskView.as_view()),
     path("progress/<str:task_id>/", ProgressView.as_view()),
 ]
