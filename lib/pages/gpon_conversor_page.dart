6e36f045a375ec105f69931e1b1279a534ac87c3
Victor <victorfariasbezerra12@gmail.com>
2025-12-26 17:11:19 -0300

    Scrolling

--- lib/pages/gpon_conversor_page.dart
+++ lib/pages/gpon_conversor_page.dart
@@ -92,147 +92,149 @@ class _GponConversorPageState extends State<GponConversorPage> {
     final bool isDisabled = file == null || isLoading;
 
     return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
-        child: Container(
-          width: 500,
-          height: 325,
-          padding: EdgeInsets.only(left: 24, right: 24, top: 24),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.grey.shade300),
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(
-                  0.1,
-                ), // Shadow color with opacity
-                offset: Offset(0, 4), // X, Y offset
-                blurRadius: 6, // Blur radius
-                spreadRadius: 0, // Spread radius
-              ),
-              BoxShadow(
-                color: Colors.black.withOpacity(0.06),
-                offset: Offset(0, 2),
-                blurRadius: 4,
-                spreadRadius: 0,
-              ),
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Conversor Gpon",
-                style: GoogleFonts.inter(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
+          child: Container(
+            width: 500,
+            height: 325,
+            padding: EdgeInsets.only(left: 24, right: 24, top: 24),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey.shade300),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(
+                    0.1,
+                  ), // Shadow color with opacity
+                  offset: Offset(0, 4), // X, Y offset
+                  blurRadius: 6, // Blur radius
+                  spreadRadius: 0, // Spread radius
                 ),
-              ),
-              SizedBox(height: 24),
-              CustomInputFile(
-                label: "Arquivo:",
-                placeholder: "Escolha um arquivo.txt",
-                file: file,
-                onPickFile: pickFile,
-              ),
-              SizedBox(height: 16),
-
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(
-                    child: CustomInput(
-                      label: "Porta:",
-                      placeholder: "EX: 1/1/1",
-                      isPassword: false,
-                      controller: portController,
-                    ),
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.06),
+                  offset: Offset(0, 2),
+                  blurRadius: 4,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Conversor Gpon",
+                  style: GoogleFonts.inter(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(width: 16),
+                ),
+                SizedBox(height: 24),
+                CustomInputFile(
+                  label: "Arquivo:",
+                  placeholder: "Escolha um arquivo.txt",
+                  file: file,
+                  onPickFile: pickFile,
+                ),
+                SizedBox(height: 16),
 
-                  // Submit Button
-                  MouseRegion(
-                    cursor: isDisabled
-                        ? SystemMouseCursors.forbidden
-                        : SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: isDisabled
-                          ? null
-                          : () async {
-                              try {
-                                setState(() {
-                                  isLoading = true;
-                                  error = null;
-                                  progress = 0;
-                                });
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: CustomInput(
+                        label: "Porta:",
+                        placeholder: "EX: 1/1/1",
+                        isPassword: false,
+                        controller: portController,
+                      ),
+                    ),
+                    SizedBox(width: 16),
 
-                                final response = await GponService.startTask(
-                                  file: file!,
-                                  port: portController.text.trim(),
-                                );
-
-                                final bodyString = await response.stream
-                                    .bytesToString();
-                                final data = jsonDecode(bodyString);
-
-                                if (response.statusCode == 200) {
-                                  final taskID = data["task_id"];
-                                  result = await runTask(taskID);
-
-                                  if (result.isNotEmpty) {
-                                    await exportTxtWithFileSaver(
-                                      filename: "resultado",
-                                      content: result["template"],
-                                    );
-                                  }
-                                } else {
+                    // Submit Button
+                    MouseRegion(
+                      cursor: isDisabled
+                          ? SystemMouseCursors.forbidden
+                          : SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: isDisabled
+                            ? null
+                            : () async {
+                                try {
                                   setState(() {
-                                    error = data['error'];
+                                    isLoading = true;
+                                    error = null;
+                                    progress = 0;
+                                  });
+
+                                  final response = await GponService.startTask(
+                                    file: file!,
+                                    port: portController.text.trim(),
+                                  );
+
+                                  final bodyString = await response.stream
+                                      .bytesToString();
+                                  final data = jsonDecode(bodyString);
+
+                                  if (response.statusCode == 200) {
+                                    final taskID = data["task_id"];
+                                    result = await runTask(taskID);
+
+                                    if (result.isNotEmpty) {
+                                      await exportTxtWithFileSaver(
+                                        filename: "resultado",
+                                        content: result["template"],
+                                      );
+                                    }
+                                  } else {
+                                    setState(() {
+                                      error = data['error'];
+                                    });
+                                  }
+                                } catch (e) {
+                                  debugPrint(e.toString());
+                                } finally {
+                                  setState(() {
+                                    isLoading = false;
                                   });
                                 }
-                              } catch (e) {
-                                debugPrint(e.toString());
-                              } finally {
-                                setState(() {
-                                  isLoading = false;
-                                });
-                              }
-                            },
-                      child: Opacity(
-                        opacity: isDisabled ? 0.5 : 1,
-                        child: Container(
-                          height: 38,
-                          decoration: BoxDecoration(
-                            color: isDisabled ? Colors.grey : Colors.black,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 24),
-                          child: Center(
-                            child: Text(
-                              "Converter",
-                              style: GoogleFonts.inter(
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
+                              },
+                        child: Opacity(
+                          opacity: isDisabled ? 0.5 : 1,
+                          child: Container(
+                            height: 38,
+                            decoration: BoxDecoration(
+                              color: isDisabled ? Colors.grey : Colors.black,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 24),
+                            child: Center(
+                              child: Text(
+                                "Converter",
+                                style: GoogleFonts.inter(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Text(
-                error ?? '',
-                style: GoogleFonts.inter(fontSize: 14, color: Colors.red),
-              ),
+                  ],
+                ),
+                Text(
+                  error ?? '',
+                  style: GoogleFonts.inter(fontSize: 14, color: Colors.red),
+                ),
 
-              SizedBox(height: 24),
+                SizedBox(height: 24),
 
-              SimpleProgressBar(progress: progress),
-            ],
+                SimpleProgressBar(progress: progress),
+              ],
+            ),
           ),
         ),
       ),
